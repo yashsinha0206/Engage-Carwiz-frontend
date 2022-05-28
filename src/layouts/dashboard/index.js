b6cfb0c99e94385ebaadb06c78c0ecfccb75538c
YASH SINHA <74976948+yashsinha0206@users.noreply.github.com>
2022-05-29 00:02:19 +0530

    Update index.js

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -9,7 +9,7 @@ import DashboardSidebar from './DashboardSidebar';
 // ----------------------------------------------------------------------
 
 const APP_BAR_MOBILE = 64;
-const APP_BAR_DESKTOP = 92;
+const APP_BAR_DESKTOP = 40;
 
 const RootStyle = styled('div')({
   display: 'flex',
